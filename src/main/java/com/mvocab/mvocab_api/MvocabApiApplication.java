d4eec7112543966ca639ce478e508834599338fe
eugenekoiner <eugenekoiner@gmail.com>
2024-01-28 19:37:25 +0700

    0.0.1

---
+++ src/main/java/com/mvocab/mvocab_api/MvocabApiApplication.java
@@ -0,0 +1,13 @@
+package com.mvocab.mvocab_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvocabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvocabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvocab/mvocab_api/MvocabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvocab.mvocab_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvocabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
