22e2c2cc02f18aa545619994e4cc90f112fcf5f0
eugenekoiner <eugenekoiner@gmail.com>
2024-06-23 23:52:44 +0700

    .

--- src/main/java/mvocab_api/security/SecurityConstant.java
+++ src/main/java/mvocab_api/security/SecurityConstant.java
@@ -2,5 +2,6 @@ package mvocab_api.security;
 
 public class SecurityConstant {
     public static final long JWT_EXPIRATION = 70000;
+    //todo: надо узнать как правильно хранить секреты
     public static final String JWT_SECRET = "LdfJHK86b778b686b7896b7b89uyGjhghjkr75kM0W6v/QpG8Wc3P4+Bl0v8f+BlzvlXyD9qx3Q2+4Cys=P4+Bl0v8f+BlzvlXyD9qx3Q2+4Cys=G8Wc3P4+Bl0v8f+BlzvlXyD9qx3Q2+4Cys=";
 }
